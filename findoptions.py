c83d884e77fdacf570207e08cf0cb70d2d8d757f
Hans Sverre Smalø <hanssverre@gmail.com>
2020-05-16 11:44:32 +0200

    Stated army builder code

--- findoptions.py
+++ findoptions.py
@@ -1,68 +1,146 @@
 from data import Team
-from data import costimized_unit
+from data import Costimized_unit
 
 
 
 def find_possible_items(costimized_unit, team):
 
-    for weapon_name,weapon in team.weapons.itmes:
-        if weapon.cost = '':
+    for weapon_name,weapon in team.weapons.items():
+        #print(weapon_name)
+        if weapon.cost == '':
             continue
-        tmp = weapon.requiered_to_buy.split(':')
+        tmp = weapon.required_to_buy.split(':')
+        
         modelORunit = tmp[0]
-        required = tmp[1].split('or')
+        required = tmp[1].split(' or ')
 
+        
+        types = [a.strip().lower() for a in costimized_unit.type_.split(',')]
+        types.append(costimized_unit.baseunitname.strip().lower())
+        
         ##check if unit meets the requirements of the item.
-        for i in required:
+        for r in required:
             still_true = True
-            if i not in costimized_unit.type_:
+            #print(weapon.name)
+            #print(r)
+            #print(types)
+            if r.strip().lower() not in types:
                     still_true = False
-                    
+
+            #print(still_true)
             if still_true:
-                break
+                break 
 
+        #print(still_true)
+            
         if not still_true:
-            break
+            continue
 
-        handed = ['1handed', '2handed', '3handed', '4handed']
+        handed = ['1handed weapon', '2handed weapon', '3handed weapon', '4handed weapon']
+        hands = ['1 hands', '2 hands', '3 hands', '4 hands']
+
         ##check if the weapon meets the requirements of the unit.
         unit_items = costimized_unit.unititems.split(',')
-        type_ = weapon.type.split(',')[1]
+        type_ = weapon.type_.split(',')[1]
+        type_ = type_.lower().strip()
         
-        if not type_ in unit_items:
+        #print(still_true)
+
+
+
+        modified_unit_items = []
+        for u in unit_items:
+            tmp = u.strip()
+            tmp = tmp[1:]
+            tmp = tmp.strip()
+            
+            modified_unit_items.append(tmp)
+
+            
+        #print(type_.strip().lower(), modified_unit_items)
+            
+        if not type_ in modified_unit_items:
             still_true = False
 
-        if costimized_unit.models > 0:
-            model_items = costimized_unit.modelitems.split(',')
-            if not type_ in model_items:
-                if type_ in handed:
-                    numberofhands_required = int(type_[0])
-                    numborof_modelhands  = 0
-                    for m in model items:
-                        if m in handed:
-                            numberof_modelhands = int(m[0])
+        ###Works so far!!!
+
+        #print('1', weapon.name)
+        if not still_true:
+            still_true = True
+            if int(costimized_unit.models) > 0:
+                model_items = costimized_unit.modelitems.split(',')
+                model_items = [a.lower().strip() for a in model_items]
+                mod = []
+                for m in model_items:
+                    if m.strip()[-5:] == 'hands':
+                        mod.append(m)
+                        continue
+                    
+                    if m[0].isdigit():
+                        if m[1].isdigit():
+                            mod.append(m[2:].strip())
+                        else:
+                            mod.append(m[1:].strip())
+                            
+                    if m.startswith('unlimited'):
+                        mod.append(m[9:].strip())
+                        
+                model_items = mod
+
+                if not type_ in model_items:
+                    if type_ in handed:
+                        numberofhands_required = int(type_[0])
+                        numberof_modelhands  = 0
+                        for m in model_items:
+                            if m in hands:
+                                #print('fuck')
+                                numberof_modelhands = int(m[0])
 
                         
-
-                    if numberofhands_requiered > numberof_modelhands:
-                        still_true = False
+                        if numberofhands_required > numberof_modelhands:
+                            still_true = False
+                        #print(still_true)
+                            
+                for replacement_name, replacement in costimized_unit.replacements.items():
+                    still_true = ture
+                    
+                    model_items = replacement.modelitems.split(',')
+                    model_items = [a.lower().strip() for a in model_items]
+                    mod = []
+                    for m in model_items:
+                        if m.strip()[-5:] == 'hands':
+                            mod.append(m)
+                            continue
+                    
+                        if m[0].isdigit():
+                            if m[1].isdigit():
+                                mod.append(m[2:].strip())
+                            else:
+                                mod.append(m[1:].strip())
+                            
+                        if m.startswith('unlimited'):
+                            mod.append(m[9:].strip())
                         
-         for replacement_name, replacement in costimized_unit.replacements.items():
-             model_items = replacement.modelitems.split(',')
-             if not type_ in model_items:
-                 if type_ in handed:
-                     numberofhands_required = int(type_[0])
-                     numborof_modelhands  = 0
-                     for m in model items:
-                         if m in handed:
-                             numberof_modelhands = int(m[0])
+                        model_items = mod
 
-                        
-
-                    if numberofhands_requiered > numberof_modelhands:
-                        still_true = False
- 
                 
-                
+                    if not type_ in model_items:
+                        if type_ in handed:
+                        
+                            numberofhands_required = int(type_[0])
+                            numberof_modelhands  = 0
+                            for m in model_items:
+                                if m in hands:            
+                                    numberof_modelhands = int(m[0])
+
+                        
+                            if numberofhands_required > numberof_modelhands:
+                                still_true = False
+
+                                
         if still_true:
             print(weapon_name)
+
+
+
+import IPython; IPython.embed()
