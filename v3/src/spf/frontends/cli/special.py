d9e50676d0b0363a27133b16383da9cddd655cab
Hans Sverre Smalø <hanssverre@gmail.com>
2026-06-21 12:29:14 +0200

    tested just and spell-fix

--- v3/src/spf/frontends/cli/special.py
+++ v3/src/spf/frontends/cli/special.py
@@ -107,7 +107,7 @@ def show_special(special_key: SpecialKey) -> None:
     Uses UMAR prefixes for U=Unit, M=Model, A=Assault, R=Range specials.
     """
     for race_name in races.list_races():
-        print(race_name)
+        stdout.print(race_name)
         # Dwarf and Gnome not updated to v3 yet.
         if race_name == "dwarf":
             continue
